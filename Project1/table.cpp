c985dd34d33943e3f547ff1982569c3a0a26462a
Michael Bergsthein <michael155750@gmail.com>
2021-08-09 09:36:55 +0300

    Add project files.

---
+++ Project1/table.cpp
@@ -0,0 +1,6 @@
+#include "Table.h"
+int main() {
+
+	Table t(2, "C:\\Users\\Omer\\Documents\\bootKamp\\2021-archive\\RFC");
+	return 0;
+}
---
+++ Project1/table.h
@@ -0,0 +1,107 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include "C:\Users\Omer\source\repos\jsoncpp\include\json\writer.h"
+class RowDescription {
+public:
+	std::string m_nameFile;
+	int m_rowNumber;
+	RowDescription();
+	RowDescription(std::string nameFile, int rowNumber)
+	{
+		m_rowNumber = rowNumber;
+		m_nameFile = nameFile;
+
+	}
+
+	std::string show() {
+
+		return "( " + m_nameFile + " " + std::to_string(m_rowNumber) + " )";
+	}
+
+
+};
+class vectorWord {
+public:
+	int m_numOfChar;
+	//int m_numberRowInFile;
+	std::string m_words;
+	RowDescription m_DescriptionVector;
+
+};
+
+class Table
+{
+private:
+
+	int m_row;
+	std::string m_path;
+	std::map<std::string, RowDescription> m_tableMap;
+public:
+	Table(int numOfchar, std::string path)
+	{
+		m_path = path;
+		std::fstream(myfile);
+
+		myfile.open(m_path);
+		int countRow = 0;
+		for (std::string line; getline(myfile, line); )//run line by line
+		{
+			countRow++;
+			for (char i = 'a'; i <= 'z'; i++)
+			{
+				for (char j = 'a'; j <= 'z'; j++)
+				{
+					vectorWord tmp;
+					tmp.m_numOfChar = 2;
+					tmp.m_words = i + j;
+					for (i = 0; i < line.length();i++)
+					{
+
+						char a = line[i];
+						char b = line[i + 1];
+						if (tmp.m_words == std::to_string(a + b))
+						{
+							tmp.m_DescriptionVector = RowDescription(path, 2);
+							m_tableMap.emplace(tmp.m_words, tmp.m_DescriptionVector);
+
+						}
+					}
+
+
+
+				}
+			}
+
+
+		}
+
+
+
+		myfile.close();
+		Json::Value jsonMap;
+		std::map<std::string, RowDescription>::const_iterator it = m_tableMap.begin(), end = m_tableMap.end();
+		for (; it != end; ++it) {
+
+			jsonMap[it->first] = "( " + it->second.m_nameFile + " " + std::to_string(it->second.m_rowNumber) + " )";
+			// ^ beware: std::to_string is C++11
+		}
+		//todo
+		//Json::Value root;
+		//root["2Char"] = "m_Table";
+		//root["Map2char"] = jsonMap; // use the Json::Value instead of mymap
+
+		//Json::StreamWriter writer = ;
+		//const std::string output = writer.write(root);
+
+
+
+	}
+	void setPath(std::string path)
+	{
+		m_path = path;
+	}
+
+};
